669ee77d0f83b967001378d69a297b47d5488f8b
Tigran Babloyan <tigran.babloyan@gmail.com>
2021-06-25 11:53:23 +0400

    fixed the models for single fat jar packaging

---
+++ generate/compile.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ..
+rm -rf generated
+basePackage="com.amazon.sellingpartner"
+models=(models/*/*)
+
+# For every model in the models/ directory, and all subdirectories:
+for model in "${models[@]}"
+do
+    # Generate a client library
+    # --input-spec $model 			:: use the current model file
+    # --lang java 				:: generate a Java library
+    # --template-dir .../ 			:: use Amazon's premade Java template files
+    # --output ../spapi 			:: put the generated library in ../spapi
+    # --invoker-package "..." 			:: put the generated code in the given package
+    # --api-package "..." 			:: put the generated api code in the given package
+    # --model-package "..." 			:: put the generated model code in the given package
+    # --group-id "..." 				:: package metadata
+    # --artifact-id "..." 			:: package metadata
+    # --additional-properties dateLibrary=java8 :: Use Java 8 date libraries
+    java -jar generate/swagger-codegen-cli.jar generate \
+        --input-spec $model \
+        --lang java \
+        --template-dir clients/sellingpartner-api-aa-java/resources/swagger-codegen/templates \
+        --output generated/spapi \
+        --invoker-package "$basePackage" \
+        --api-package "$basePackage.api" \
+        --model-package "$basePackage.model" \
+        --group-id "com.amazon" \
+        --artifact-id "selling-partner-api" \
+        --additional-properties dateLibrary=java8
+done
