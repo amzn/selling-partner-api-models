4584c631b19e777d5ccf2ef49b5794ca71b61bf5
Sean Howard <cshoward@microsoft.com>
2023-02-09 15:47:08 -0800

    Fixing hardcoded path

--- clients/sellingpartner-api-aa-csharp/src/Amazon.SellingPartnerAPIAA/resources/swagger-codegen/Post-Build.bat
+++ clients/sellingpartner-api-aa-csharp/src/Amazon.SellingPartnerAPIAA/resources/swagger-codegen/Post-Build.bat
@@ -2,7 +2,7 @@ java ^
 -jar C:\swagger-codegen\swagger-codegen-cli.jar generate ^
 -l csharp ^
 -i ..\..\..\..\models\orders-api-model\ordersV0.json ^
--t C:\Users\cshoward\source\repos\selling-partner-api-models\clients\sellingpartner-api-aa-csharp\src\Amazon.SellingPartnerAPIAA\resources\swagger-codegen\templates ^
+-t ..\..\..\..\clients\sellingpartner-api-aa-csharp\src\Amazon.SellingPartnerAPIAA\resources\swagger-codegen\templates ^
 -o ..\..\..\..\clients\sellingpartner-api-aa-csharp\client ^
 -c ..\..\..\..\clients\sellingpartner-api-aa-csharp\src\Amazon.SellingPartnerAPIAA\resources\swagger-codegen\config.json ^
 --ignore-file-override ..\..\..\..\clients\sellingpartner-api-aa-csharp\src\Amazon.SellingPartnerAPIAA\resources\swagger-codegen\.swagger-codegen-ignore ^
